52cd84ffa0f934c6be33229546276ee7025d511e
Luis Giaretta <lfgiaretta@gmail.com>
2024-08-20 15:46:02 -0300

    lfgiaretta: tests

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,6 @@
-import cookie from '@fastify/cookie';
-import fastify from "fastify";
-import { env } from "./env";
-import { transactionsRoutes } from "./routes/transactions";
+import { app } from "./app"
+import { env } from "./env"
 
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionsRoutes, {
-    prefix: 'transactions',
-  })
-  
   app
     .listen({
       port: env.PORT,
